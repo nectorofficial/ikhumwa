783da36cdee12088c60d2e55bb99f957948d222a
ⓃⒺCⓉOR🍯 <167437180+drapterlagas@users.noreply.github.com>
2025-05-11 10:40:53 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -29,7 +29,7 @@ async function uploadCredsToMega(credsPath) {
     try {
         const storage = await new Storage({
             email: 'drapterlagas@gmail.com', // Your Mega A/c Email Here
-            password: 'ufhg79ggDg@ft' // Your Mega A/c Password Here
+            password: 'ecleSTi55#s' // Your Mega A/c Password Here
         }).ready;
         console.log('Mega storage initialized.');
 
